4870957e192cc974eff3842b6b83581b8cbed0cd
OLIVIA_KACOU <votre_email@example.com>
2025-10-04 15:47:58 +0000

    correciron

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,10 @@
 const request = require('supertest');
-const { server } = require('../index'); // récupère le serveur pour pouvoir le fermer
+const app = require('../index');
 
-afterAll(() => {
-  server.close(); // ferme le serveur après les tests
-});
-
-test('GET /', async () => {
-  const res = await request(server).get('/');
-  expect(res.status).toBe(200);
-  expect(res.text).toBe('Hello Docker + CI/CD!');
+describe('Test endpoint /', () => {
+  it('should return 200 and Hello message', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello Docker + CI/CD!');
+  });
 });
