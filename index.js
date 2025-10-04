1caca544daf8e7b21000e95ba21aedb723f79789
OLIVIA_KACOU <votre_email@example.com>
2025-10-04 15:45:23 +0000

    correction

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 // index.js
 const express = require('express');
 const app = express();
-const PORT = 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Hello Docker + CI/CD!');
 });
 
-// Ne démarrer le serveur que si le fichier est exécuté directement
-// (pas lorsqu'il est importé par les tests)
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}');
-  });
-}
 
 module.exports = app;
